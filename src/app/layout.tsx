63c559fdd96442d1d763f90d72ab48679a599982
Eduard May <eduardmay537@gmail.com>
2025-10-07 05:17:56 +0000

    ay amigo no me ayudas en nada, ya llevo mucho tiempo tratando que lo hag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        <link rel="stylesheet" href="/print.css" media="print" />
       </head>
       <body
         className={cn(
