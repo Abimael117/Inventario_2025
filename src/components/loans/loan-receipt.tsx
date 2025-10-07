e2288c3b79457598d044106cd25d798ae60ed572
Eduard May <eduardmay537@gmail.com>
2025-10-07 03:44:14 +0000

    este es el qu eno funciona

--- src/components/loans/loan-receipt.tsx
+++ src/components/loans/loan-receipt.tsx
@@ -60,8 +60,7 @@ const PrintableContent = React.forwardRef<HTMLDivElement, { loan: Loan; entregad
                     </div>
                     <div className="text-center">
                         <div className="border-b border-gray-400 w-3/4 mx-auto">&nbsp;</div>
-                        <p className="mt-2 text-sm font-semibold">Recibido por</p>
-                         <input
+                        <p className="mt-2 text-sm font-semibold">Recibido por</p>                         <input
                         type="text"
                         placeholder="Nombre de quien recibe"
                         className="w-3/4 mx-auto border-0 text-center text-sm focus:outline-none focus:ring-0 bg-transparent text-gray-500"
