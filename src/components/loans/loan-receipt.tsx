9a224a3b1b3e16d00371ea4ffda913c1280e9181
Eduard May <eduardmay537@gmail.com>
2025-10-07 14:11:43 +0000

    marca esto:

--- src/components/loans/loan-receipt.tsx
+++ src/components/loans/loan-receipt.tsx
@@ -6,6 +6,7 @@ import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import Image from 'next/image';
 import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
 
 export function LoanReceipt({ loan, className }: { loan: Loan, className?: string }) {
   const [entregadoPor, setEntregadoPor] = React.useState('');
