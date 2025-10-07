2d2353888a3b5a993d3122bde8467fac6346621d
Eduard May <eduardmay537@gmail.com>
2025-10-07 03:15:07 +0000

    nooooooooooooooooooooooooooooooooooo

--- src/components/loans/loan-receipt.tsx
+++ src/components/loans/loan-receipt.tsx
@@ -55,7 +55,6 @@ export function LoanReceipt({ loan }: { loan: Loan }) {
 
                 <div className="mt-24 grid grid-cols-2 gap-8 pt-12">
                     <div className="text-center">
-                        <p className="mt-1 text-sm font-semibold">Entregado por</p>
                         <input
                         type="text"
                         placeholder="Nombre y Firma"
@@ -63,9 +62,9 @@ export function LoanReceipt({ loan }: { loan: Loan }) {
                         value={entregadoPor}
                         onChange={(e) => setEntregadoPor(e.target.value)}
                         />
+                        <p className="mt-1 text-sm font-semibold">Entregado por</p>
                     </div>
                     <div className="text-center">
-                        <p className="mt-1 text-sm font-semibold">Recibido por</p>
                         <input
                         type="text"
                         placeholder="Nombre y Firma"
@@ -73,6 +72,7 @@ export function LoanReceipt({ loan }: { loan: Loan }) {
                         value={recibidoPor}
                         onChange={(e) => setRecibidoPor(e.target.value)}
                         />
+                        <p className="mt-1 text-sm font-semibold">Recibido por</p>
                     </div>
                 </div>
             </main>
