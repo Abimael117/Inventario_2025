bb01bc14c3c62533fdb1dc089cbd0d192c3566e3
Eduard May <eduardmay537@gmail.com>
2025-10-07 05:09:51 +0000

    alch no se que mas decirte para tener el tamaño bien

--- src/components/loans/loan-receipt.tsx
+++ src/components/loans/loan-receipt.tsx
@@ -23,15 +23,15 @@ export function LoanReceipt({ loan }: { loan: Loan }) {
         className="receipt-container bg-white text-black font-sans"
       >
         <header className="flex justify-between items-center pb-4 border-b-2" style={{ borderColor: '#C0A0A0' }}>
-          <div className="flex items-center justify-start w-1/4">
+          <div className="flex items-center justify-start">
             <Image src="https://escarcega.gob.mx/escarcega.png" alt="Escudo de Escárcega" width={120} height={120} data-ai-hint="logo government" />
           </div>
-          <div className="text-center text-sm font-semibold w-1/2">
+          <div className="text-center text-sm font-semibold">
             <p>HONORABLE AYUNTAMIENTO</p>
             <p>DE ESCÁRCEGA</p>
             <p>2024-2027</p>
           </div>
-          <div className="flex items-center justify-end w-1/4">
+          <div className="flex items-center justify-end">
             <Image src="https://tse1.mm.bing.net/th/id/OIP.W6OOgA_8g2-y3CIw54Uk6gHaEK?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3" alt="Logo Gobierno de Escárcega" width={150} height={84} data-ai-hint="logo city" />
           </div>
         </header>
