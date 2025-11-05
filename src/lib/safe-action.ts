580b5458b11c9cc6d6e88da8208fbcade0b25e62
Eduard May <eduardmay537@gmail.com>
2025-11-05 18:13:03 +0000

    Try fixing this error: `Runtime Error: TypeError: (0 , {imported module

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -2,4 +2,6 @@ import { createSafeActionClient } from 'next-safe-action';
 
 // This is our action client.
 // It can be used to create safe server actions.
-export const action = createSafeActionClient();
+const actionClient = createSafeActionClient();
+
+export const action = actionClient;
